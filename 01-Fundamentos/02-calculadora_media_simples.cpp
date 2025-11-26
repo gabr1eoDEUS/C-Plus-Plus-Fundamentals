1a18f539e20e3b563a6ee7366caeece02b448ded
gabr1eoDEUS <exe00011000@gmail.com>
2025-11-26 12:33:53 -0300

    Add simple average calculator in C++

---
+++ 01-Fundamentos/02-calculadora_media_simples.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+	const float PESO_TRABALHO = 0.40f;
+	const float PESO_PROVA = 0.60f;
+	float notaTrabalho = 0.0, notaProva = 0.0;
+	cout << "Digite a nota do trabalho: ";
+	cin >> notaTrabalho;
+	cout << "Digite a nota da prova: ";
+	cin >> notaProva;
+  cout << "--------------------\n";
+	float notaFinal = (notaTrabalho * PESO_TRABALHO) + (notaProva * PESO_PROVA);
+	cout << "A nota final do aluno é: " << notaFinal << endl;
+	return 0;
+};
