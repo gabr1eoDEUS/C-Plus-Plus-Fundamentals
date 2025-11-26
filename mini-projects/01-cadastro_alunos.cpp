5c7dc5eadf5eee47f8ea3c33b8a4709f1c219875
gabr1eoDEUS <exe00011000@gmail.com>
2025-11-26 14:11:31 -0300

    Add struct Aluno and basic setup in cadastro_alunos.cpp

--- mini-projects/01-cadastro_alunos.cpp
+++ mini-projects/01-cadastro_alunos.cpp
@@ -1,1 +1,12 @@
-
+#include <iostream>
+#include <string>
+using namespace std;
+struct Aluno {
+	string nome = "";
+	int matricula = 0;
+	float media = 0.0;
+};
+int main() {
+	const int TAM = 3;
+    return 0;
+}
