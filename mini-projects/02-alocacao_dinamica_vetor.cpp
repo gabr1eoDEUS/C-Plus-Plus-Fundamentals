441ac5d58cbcacba0d57b6fe5714fae5e3a67338
gabr1eoDEUS <exe00011000@gmail.com>
2025-11-26 14:48:46 -0300

    Implement grade reading function for dynamic array
    Added function to read grades into dynamically allocated array.

--- mini-projects/02-alocacao_dinamica_vetor.cpp
+++ mini-projects/02-alocacao_dinamica_vetor.cpp
@@ -2,14 +2,22 @@
 #include <string>
 using namespace std;
 float* alocarNotas(int tamanho);
+void lerNotas(float* notas, const int tamanho);
 int main() {
     float* notas = nullptr;
     int tamanho = 0;
     cout << "Digite o tamanho da turma: ";
     cin >> tamanho;
     notas = alocarNotas(tamanho);
+    lerNotas(notas, tamanho);
     return 0;
 }
 float* alocarNotas(int tamanho) {
     return new float[tamanho];
+}
+void lerNotas(float* notas, const int tamanho) {
+    for (int i = 0; i < tamanho; i++) {
+        cout << "Digite a nota na posicao [ " << (i + 1) << " ]: ";
+        cin >> notas[i];
+    }
 }
