c21ad552924fbc01960ee6c189e8b93d4a8a726d
gabr1eoDEUS <exe00011000@gmail.com>
2025-11-26 14:17:45 -0300

    Implement student registration function
    Added a function to register a new student with input prompts.

--- mini-projects/01-cadastro_alunos.cpp
+++ mini-projects/01-cadastro_alunos.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 using namespace std;
+void cadastrarAluno(Aluno& novoAluno);
 struct Aluno {
 	string nome = "";
 	int matricula = 0;
@@ -9,4 +10,14 @@ struct Aluno {
 int main() {
 	const int TAM = 3;
     return 0;
+}
+void cadastrarAluno(Aluno &novoAluno) {
+	cout << "Nome do Aluno: ";
+	getline(cin, novoAluno.nome);
+	cout << "Matricula: ";
+	cin >> novoAluno.matricula;
+	cin.ignore();
+	cout << "Media: ";
+	cin >> novoAluno.media;
+	cin.ignore();
 }
