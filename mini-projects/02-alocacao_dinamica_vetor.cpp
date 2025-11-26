67d14a8a2ff19bdf58ad57c5dd3db6b2e54e4b55
gabr1eoDEUS <exe00011000@gmail.com>
2025-11-26 15:02:27 -0300

    Add functions to calculate average and free memory

--- mini-projects/02-alocacao_dinamica_vetor.cpp
+++ mini-projects/02-alocacao_dinamica_vetor.cpp
@@ -3,6 +3,8 @@
 using namespace std;
 float* alocarNotas(int tamanho);
 void lerNotas(float* notas, const int tamanho);
+float calcularMedia(const float* notas, const int tamanho);
+void liberarMemoria(float* &notas);
 int main() {
     float* notas = nullptr;
     int tamanho = 0;
@@ -10,6 +12,9 @@ int main() {
     cin >> tamanho;
     notas = alocarNotas(tamanho);
     lerNotas(notas, tamanho);
+    float media = calcularMedia(notas, tamanho);
+    cout << "A media das notas e: " << media << endl;
+    liberarMemoria(notas);
     return 0;
 }
 float* alocarNotas(int tamanho) {
@@ -20,4 +25,15 @@ void lerNotas(float* notas, const int tamanho) {
         cout << "Digite a nota na posicao [ " << (i + 1) << " ]: ";
         cin >> notas[i];
     }
+}
+float calcularMedia(const float* notas, const int tamanho) {
+    double soma = 0.0;
+    for (int i = 0; i < tamanho; i++) {
+        soma += notas[i];
+    }
+    return soma / tamanho;
+}
+void liberarMemoria(float* &notas) {
+    delete[] notas;
+    notas = nullptr;
 }
