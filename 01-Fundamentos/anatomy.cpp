18f515c1f059a7411dc3a83c73470fbe1a1778d3
gabr1eoDEUS <exe00011000@gmail.com>
2025-11-29 13:42:44 -0300

    Create anatomy.cpp

---
+++ 01-Fundamentos/anatomy.cpp
@@ -0,0 +1,19 @@
+// 1. Diretiva de Pré-processador:
+// Inclui o cabeçalho iostream, essencial para usar comandos de
+// Entrada (cin) e Saída (cout).
+#include <iostream> 
+
+// 2. Assinatura da Função Principal (Ponto de Entrada):
+// int: Declara que esta função retornará um valor inteiro (código de status).
+// main(): O nome que o sistema operacional procura para iniciar a execução.
+int main() { 
+
+    // 3. Bloco de Código:
+    // As chaves delimitam o que será executado pela função main().
+    
+    // 4. Retorno de Status:
+    // return 0; : Indica que o programa terminou com SUCESSO.
+    // O ponto e vírgula (;) é obrigatório para encerrar a instrução.
+    return 0; 
+
+} // Fim do Bloco de Código
