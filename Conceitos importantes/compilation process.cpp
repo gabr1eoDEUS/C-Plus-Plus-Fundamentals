3a06e44be55173738ef3ee7e3f9aaa2de8434f5b
gabr1eoDEUS <exe00011000@gmail.com>
2025-12-04 13:29:23 -0300

    Add compilation process example in C++

---
+++ Conceitos importantes/compilation process.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+// O #include é uma diretiva do Pré-processador.
+// Ela é a PRIMEIRA fase do Processo de Compilação.
+
+using namespace std;
+
+// Este é o código-fonte (Source Code) que será COMPILADO
+// em código de máquina (objeto) e, finalmente, LIGADO (Linked)
+// com as bibliotecas (como o iostream) para criar o executável.
+int main() {
+    cout << "Fase de Compilação Concluída (Simbólica)." << endl;
+    return 0;
+}
